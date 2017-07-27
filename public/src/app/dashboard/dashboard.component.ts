23fc0dd3c87a0af913423663616ca8c6e2b149b1
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-07-27 12:02:02 -0500

    dash componet working

--- public/src/app/dashboard/dashboard.component.ts
+++ public/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Http } from "@angular/http";
 import { UserService } from "../user.service";
+import { BucketService } from "../bucket.service";
 import { Router } from "@angular/router";
 
 import "rxjs";
@@ -11,11 +12,37 @@ import "rxjs";
   styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
-  currentUser = {};
-  constructor(private _userService: UserService, private router: Router) {}
+  currentUser = { _id: "" };
+  newItem = { user: "" };
+  errors: string[] = [];
+
+  constructor(
+    private _bucketService: BucketService,
+    private _userService: UserService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.getCurrentUser();
+  }
+
+  createItem() {
+    this.errors = [];
+    console.log(this.newItem);
+    this.newItem.user = this.currentUser._id;
+    return this._bucketService
+      .create(this.newItem)
+      .then(bucket => {
+        if (bucket.errors) {
+          for (let key in bucket.errors) {
+            let error = bucket.error[key];
+            this.errors.push(error.message);
+          }
+        } else {
+          this.router.navigateByUrl("/dashboard");
+        }
+      })
+      .catch(err => console.log(err));
   }
 
   getCurrentUser() {
